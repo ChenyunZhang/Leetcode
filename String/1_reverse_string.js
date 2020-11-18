d39bac35a7262ff0bb5ea7e333f983e03064d1b5
Chenyun Zhang <morningchenyun@gmail.com>
2020-11-18 15:07:54 -0500

    day 2

--- String/1_reverse_string.js
+++ String/1_reverse_string.js
@@ -1,8 +1,9 @@
 var reverseString = function (s) {
   for (let i = s.length-1; i >= 0; i--) {
-    s.push(s.splice(i,1)[0])
+    s.push(s.splice(i,1))
   }
-  return s
+  console.log(s.flat())
+  return s.flat()
 };
 
 reverseString(["H", "a", "n", "n", "a", "h"]);
