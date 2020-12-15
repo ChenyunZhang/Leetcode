e08ca008698f856df8b36a7abc9894a746a5a797
Chenyun Zhang <morningchenyun@gmail.com>
2020-12-15 09:24:42 -0500

    reverse string recursively

---
+++ recursion/reverese_string.js
@@ -0,0 +1,19 @@
+// write a function that takes a string, return a new string in reverse
+function reverse(str) {
+  let new_str = []
+  const helper = (str) =>{
+    if(str.length === 0){return}
+    let temp = str[0]
+    str = str.substring(1)
+    helper(str)
+    new_str.push(temp)
+  }
+  helper(str)
+  return new_str.join("")
+}
+
+console.log(reverse("abc"))
+
+  // add whatever parameters you deem necessary - good luck!
+  // base case: when input string is empty
+  // reccurrance relation: keep shifting the first letter in the string.
