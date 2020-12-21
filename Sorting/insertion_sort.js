486f5f1237912e97a93e59dd0c3914d5e310a43a
Chenyun Zhang <morningchenyun@gmail.com>
2020-12-20 20:21:02 -0500

    zip

---
+++ Sorting/insertion_sort.js
@@ -0,0 +1,7 @@
+// insertion sort builds up the sort by gradually creating a larger left half which is always sorted
+// start by picking the second element in the array
+// compare the second element with the one before it and swap if nexessary
+// continue to the next Element,emt and if it is the incorrect order
+// iterate throught the sorrted portion to the place the element in the correct place
+// repeat until the array is sorted
+
