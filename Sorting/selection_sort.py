3a9cdb7f0ef33fc2921434aeb334b3100708be62
Chenyun Zhang <morningchenyun@gmail.com>
2020-12-20 20:50:24 -0500

    zip and enumerable

--- Sorting/selection_sort.py
+++ Sorting/selection_sort.py
@@ -1,25 +1,60 @@
-Zip and Enumerate 
+x_coord = [23, 53, 2, -12, 95, 103, 14, -5]
+y_coord = [677, 233, 405, 433, 905, 376, 432, 445]
+z_coord = [4, 16, -6, -42, 3, -6, 23, -1]
+labels = ["F", "J", "A", "Q", "Y", "B", "W", "X"]
 
-zip and enumerate are useful built-in functions that can come in handy when dealing with loops. 
+points = []
+for point in zip(labels, x_coord, y_coord, z_coord):
+    points.append("{}: {}, {}, {}".format(*point))
 
-Zip 
+for point in points:
+    print(point)
+# Output:
+# F: 23, 677, 4
+# J: 53, 233, 16
+# A: 2, 405, -6
+# Q: -12, 433, -42
+# Y: 95, 905, 3
+# B: 103, 376, -6
+# W: 14, 432, 23
+# X: -5, 445, -1
+# Notice here, the tuple was unpacked using * in the format method. This can help make your code cleaner!
 
-zip returns an iterator that combines multiple iterables into one sequence of tuples. Each tuple contains the elements in that position from all the iterables. For example, printing 
+# Quiz Solution: Zip Lists to a Dictionary
+# cast_names = ["Barney", "Robin", "Ted", "Lily", "Marshall"]
+# cast_heights = [72, 68, 72, 66, 76]
 
-list(zip(['a', 'b', 'c'], [1, 2, 3])) would output [('a', 1), ('b', 2), ('c', 3)]. 
+# cast = dict(zip(cast_names, cast_heights))
+# print(cast)
+# Output:
+# The order of elements in this output may vary since dictionaries are unordered.
 
-Like we did for range() we need to convert it to a list or iterate through it with a loop to see the elements. 
+# {'Lily': 66, 'Barney': 72, 'Marshall': 76, 'Ted': 72, 'Robin': 68}
+# Quiz Solution: Unzip Tuples
+# cast = (("Barney", 72), ("Robin", 68), ("Ted", 72), ("Lily", 66), ("Marshall", 76))
 
-You could unpack each tuple in a for loop like this. 
+# names, heights = zip(*cast)
+# print(names)
+# print(heights)
+# Output:
+# ('Barney', 'Robin', 'Ted', 'Lily', 'Marshall')
+# (72, 68, 72, 66, 76)
+# Quiz Solution: Transpose with Zip
+# data = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (9, 10, 11))
 
-letters = ['a', 'b', 'c'] nums = [1, 2, 3] for letter, num in zip(letters, nums): print("{}: {}".format(letter, num))  
+# data_transpose = tuple(zip(*data))
+# print(data_transpose)
+# Output:
+# ((0, 3, 6, 9), (1, 4, 7, 10), (2, 5, 8, 11))
+# This is a useful trick to know!
 
-In addition to zipping two lists together, you can also unzip a list into tuples using an asterisk. 
+# Quiz Solution: Enumerate
+# cast = ["Barney Stinson", "Robin Scherbatsky", "Ted Mosby", "Lily Aldrin", "Marshall Eriksen"]
+# heights = [72, 68, 72, 66, 76]
 
-some_list = [('a', 1), ('b', 2), ('c', 3)] letters, nums = zip(*some_list)  
+# for i, character in enumerate(cast):
+#     cast[i] = character + " " + str(heights[i])
 
-This would create the same letters and nums tuples we saw earlier. 
-
-Enumerate 
-
-enumerate is a built in function that returns an iterator of tuples containing indices and values of a list. You'll often use this when you want the index along with each element of an iterable in a loop. 
+# print(cast)
+# Output:
+# ['Barney Stinson 72', 'Robin Scherbatsky 68', 'Ted Mosby 72', 'Lily Aldrin 66', 'Marshall Eriksen 76']
