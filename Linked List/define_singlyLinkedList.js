05a5fe9fb210eec8baed75db42c5f408284679aa
Chenyun Zhang <morningchenyun@gmail.com>
2020-12-21 10:29:09 -0500

    linked list shift,unshift,pop,push

--- Linked List/define_singlyLinkedList.js
+++ Linked List/define_singlyLinkedList.js
@@ -12,12 +12,56 @@ class SinglyLinkedList{
         this.length = 0
     }
     push(val){
-        node = new SinglyLinkedList()
+        let newNode = new Node(val)
         if(!this.head){
-            node = this.head
+            this.head = newNode
+            this.tail=this.head
         }else{
-            node = this.tail.next
+            this.tail.next = newNode
+            this.tail = newNode
         }
-        this.length+=1
+        this.length++
+        return this;
+    }
+
+    // traverse(){
+    //     let current = this.head
+    //     while(current){
+    //         console.log(current.val)
+    //         currnet=current.next;
+    //     }
+    // }
+
+    pop(){
+        if(!this.head) return undefined
+        let current = this.head
+        let newTail = current
+        while(current.next){
+            newTail = current
+            current = current.next
+        }
+        this.tail = newTail
+        this.tail.next = null
+        this.length--
+        if(this.length === 0){
+            this.head = null
+            this.tail = null
+        }
+        return current
+    }
+
+    shift(){
+        if(!this.head) return undefined
+        let current = this.head
+        this.head =current.next
+        this.length --
+        if(this.length === 0){
+            this.tail = null
+        }
+        return current
+    }
+    unshift(value){
+        let newNode = new Node(val = value)
+        
     }
 }
