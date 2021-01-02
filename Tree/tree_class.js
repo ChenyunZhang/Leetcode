5ffc5755b2f0f608f75c1129c4094277cd4c5e89
Chenyun Zhang <morningchenyun@gmail.com>
2021-01-02 04:10:27 -0500

    bst

--- Tree/tree_class.js
+++ Tree/tree_class.js
@@ -5,11 +5,35 @@ class Node{
     }
 
     add(data){
-        const node = new Node(data)
         this.children.push(new Node(data))
     }
 
     remove(data){
-        
+        this.chirdren.filter(node=>{
+            return node.data !=data
+        })
+    }
+}
+
+class Tree{
+    constructor(){
+        this.root = null
+    // every time you see width, it's bf
+    traverseBF(fn){
+        const arr = [this.root]
+        while(arr.length){
+            const node = arr.shift()
+            arr.push(...node.children)
+            fn(node)
+        }
+    }
+
+    traverseDF(fn){
+        const arr = [this.root]
+        while(arr.length){
+            const node = arr.shift()
+            arr.unshift(...node.children)
+            fn(node)
+        }
     }
 }
